69fe784c7a2778c141605770c0ae39ab1a60fc04
Nikhil007 <nikhil.gurram02@gmail.com>
2026-05-08 16:34:56 -0400

    secure firebase config with .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,24 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBREva2czcU_oHKCAaJ1Qk815Cw6O68sx4",
-  authDomain: "cyclestock-f94f4.firebaseapp.com",
-  projectId: "cyclestock-f94f4",
-  storageBucket: "cyclestock-f94f4.firebasestorage.app",
-  messagingSenderId: "1033189799460",
-  appId: "1:1033189799460:web:d7ee9ea5fe5a4bddfd8ab1",
-  measurementId: "G-70D3W3V4FB"
+  apiKey:            import.meta.env.VITE_API_KEY,
+  authDomain:        import.meta.env.VITE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider("6Ldb7t8sAAAAABFVpMW6pkVZQViG0Ard8r_CG0Pr"),
-  isTokenAutoRefreshEnabled: true
-});
-
 export const db      = getFirestore(app);
 export const auth    = getAuth(app);
 export const storage = getStorage(app);
